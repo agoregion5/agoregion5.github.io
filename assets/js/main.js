c9d4cd21a564b572ddf8b566095ce94090bd795a
AGO Region5 <agoregion5@gmail.com>
2021-07-05 16:34:49 +0700

    Fix Git Error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function changeFrameSrc(code) {
     var frame = document.getElementById('egpFrame');
-    var url = "https://process.gprocurement.go.th/egp2procmainWeb/jsp/public_announ_search.jsp?homeflag=S&deptsubid=%CA%D3%B9%D1%A1%A7%D2%B9%CD%D1%C2%A1%D2%C3%C0%D2%A4%205&announcetype=" + code;
+    var url = "http://process.gprocurement.go.th/egp2procmainWeb/jsp/public_announ_search.jsp?homeflag=S&deptsubid=%CA%D3%B9%D1%A1%A7%D2%B9%CD%D1%C2%A1%D2%C3%C0%D2%A4%205&announcetype=" + code;
     frame.src = url;
     frame.contentWindow.location.reload();
 }
